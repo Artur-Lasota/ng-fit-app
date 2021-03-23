1ea97c58de1cf6fabfc075a276dcca221fd31d4f
Manchox <manchox931@gmail.com>
2021-03-23 21:10:00 +0100

    add settings

--- ng-fit-app/src/app/app.module.ts
+++ ng-fit-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
