6804133a0d3011424101b452395debbecaf93a16
alasota <alasota@sii.pl>
2021-07-24 21:12:35 +0200

    fix db.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('./src/server/db.json');
 const middlewares = jsonServer.defaults();
 const db = require('./db.json');
 const fs = require('fs');
