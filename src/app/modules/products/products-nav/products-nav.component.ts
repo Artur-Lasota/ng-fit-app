dc4855a78e7d23de709e0666f1b34bf2b5b6b962
alasota <alasota@sii.pl>
2021-08-24 22:53:27 +0200

    add reset css

---
+++ src/app/modules/products/products-nav/products-nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-products-nav',
+    templateUrl: './products-nav.component.html',
+    styleUrls: ['./products-nav.component.scss']
+})
+
+export class ProductsNavigationComponent {
+
+
+    constructor() {
+    }
+}
